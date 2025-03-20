d5bf5f5c354d60ef7b00f03cecff273b707b6aa0
“latiadalia” <lamnguyen.work03@gmail.com>
2025-03-20 19:09:11 +0700

    BT thuc Hanh

---
+++ demoMVC/MvcMovie/Migrations/20250320120512_InitialCreate.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "Persons");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Persons",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PersonId = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    FullName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Address = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    Salary = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Persons");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Persons",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Persons",
+                type: "TEXT",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeId",
+                table: "Persons",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
